c893fcdb03ab416e44700f3a1dec069cdc8fd9ac
Amin Memariani <amin90memar@gmail.com>
2024-11-14 10:24:53 +0330

    added route string to setting page

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class SettingPage extends StatefulWidget {
-  
+  static const route = "setting";
+
   const SettingPage({super.key});
 
   @override
   State<SettingPage> createState() => _SettingPageState();
 }
-
 
 class _SettingPageState extends State<SettingPage> {
   @override

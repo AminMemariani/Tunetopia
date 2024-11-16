1567ba9e7ca978141c3cb152ab17c17bdbd03e82
Amin Memariani <amin90memar@gmail.com>
2024-11-17 01:24:50 +0330

    appbar extracted to a stl widget

---
+++ lib/pages/widgets/appbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key, required this.title, this.actions});
+
+  final String title;
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.amber,
+      centerTitle: true,
+      title: Text(title),
+      actions: actions,
+    );
+  }
+}

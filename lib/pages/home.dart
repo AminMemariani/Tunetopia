da25bca790e9aadaabd44b043e71bf38bb8242fd
Amin Memariani <amin90memar@gmail.com>
2024-11-24 22:04:14 +0330

    open files added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 import 'package:music_player/pages/widgets/my_drawer.dart';
@@ -21,6 +22,18 @@ class _HomePageState extends State<HomePage> {
         title: MyStyles.appName,
         backgroundColor: Theme.of(context).colorScheme.primary,
         elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                  allowMultiple: true,
+                  type: FileType.custom,
+                  allowedExtensions: ['mp3', 'wav', 'ogg', 'aac'],
+                );
+                debugPrint("result: ${result.toString()}");
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       drawer: const MyDrawer(),
       body: Padding(padding: const EdgeInsets.all(12), child: HomeItem()),

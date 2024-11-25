3e79e39b74a8d001704f533adb748a304a707fd1
Amin Memariani <amin90memar@gmail.com>
2024-11-25 23:53:58 +0330

    Added addSongs method to songs provider

--- lib/providers/songs.dart
+++ lib/providers/songs.dart
@@ -1,5 +1,16 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class Songs with ChangeNotifier {
   final List<String> songs = ["11111", "222222", "3333333"];
+
+  Future<void> addSongs(String files) async {
+    RegExp regex = RegExp(r"name: ([^\s,]+)");
+    Match? match = regex.firstMatch(files);
+    if (match != null) {
+      String filename = match.group(1)!;
+      print(filename); // Output: hope.mp3
+    } else {
+      print("Filename not found.");
+    }
+  }
 }
